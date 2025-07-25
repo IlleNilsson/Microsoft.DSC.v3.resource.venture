dc76e11ab211a2ba09bfa230d4ed27c9e113908c
Ilian Nilsson <ilian.nilsson@visity.se>
2025-07-25 13:40:26 +0200

    Resolved format problem, spaces

--- .resources/windows/scratchPad.ps1
+++ .resources/windows/scratchPad.ps1
@@ -1,8 +1,7 @@
 pwsh -NoLogo -NonInteractive -NoProfile -NoProfileLoadTime -InputFormat Text -OutputFormat Text -WindowStyle Normal -CommandWithArgs @'
     $vars = Get-Variable
 
-    Write-Host "Dumplings $args[1] dummy jump"
+    Write-Host "Dumplings '$($args[0])' dummy jump '$($args[1])'"
 
-    # return ($vars | ConvertTo-Json)
-'@
-Sabs Sucks
+    return ($vars | ConvertTo-Yaml)
+'@ Arg1 Arg2
