1e041d229dd3bf90bd84790e4af707fa03fa3840
Ilian Nilsson <ilian.nilsson@visity.se>
2025-08-02 06:28:07 +0200

    Added required

--- .resources/windows/scriptLibrary.ps1
+++ .resources/windows/scriptLibrary.ps1
@@ -1,35 +1,39 @@
 # Get Feature State
+#Requires -Version 7.5
+#Requires -PSEdition Core
+#Requires -RunAsAdministrator
+
 $osType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
 $feature = $null
 switch ($osType) {
-    1 { 
-        $feature = Get-WindowsOptionalFeature -Online -FeatureName $Name 
-    }
-    default {
-        $feature = Get-WindowsFeature -Name $Name
-    }  
+    1       { $feature = Get-WindowsOptionalFeature -Online -FeatureName $name }
+    default { $feature = Get-WindowsFeature -Name $name }  
 }
 
 if (!$feature) { 
-    throw New-Object ArgumentException "Feature '$Name' not found"
+    throw New-Object ArgumentException "Feature '$name' not found"
 }
 
 return $feature | ConvertTo-Yaml
 
 # Test Feature
+#Requires -Version 7.5
+#Requires -PSEdition Core
+#Requires -RunAsAdministrator
+
 $osType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
 switch ($osType) {
-    1 { 
-    }
-    default {
-    }  
+    1       { }
+    default { }  
 }
 
 # Set Feature
+#Requires -Version 7.5
+#Requires -PSEdition Core
+#Requires -RunAsAdministrator
+
 $osType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
 switch ($osType) {
-    1 { 
-    }
-    default {
-    }  
+    1       { }
+    default { }  
 }
